c4fb53569b8ee1ecd25f0886f44494ccc1210fb4
Carlos <carlos.e.santaniello@gmail.com>
2020-10-07 16:23:03 +0200

    Construir una app

---
+++ scripts1.js
@@ -0,0 +1,8 @@
+function AlumnosController($scope){
+    $scope.alumnos=[
+            {nombre:"Juan Blanco", telefono: "1234567890", curso:"Segundo ESO"},
+            {nombre:"Rosa Luxemburgo", telefono: "0987654321", curso:"Primero ESO"},
+            {nombre:"Alberto Herrera", telefono: "1122334455", curso:"Segundo ESO"},
+            {nombre:"Ana Mariño", telefono: "6677889900", curso:"Tercero ESO"}
+            ];
+    }
