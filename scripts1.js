cc6fb8c2b8374328f46002cdbedbe5ae1ecb0b07
Carlos <carlos.e.santaniello@gmail.com>
2020-10-08 13:10:52 +0200

    Mostrar/ocultar formulario

--- scripts1.js
+++ scripts1.js
@@ -1,13 +1,23 @@
 function AlumnosController($scope){
-    $scope.alumnos=[
-            {nombre:"Juan Blanco", telefono: "1234567890", curso:"Segundo ESO"},
-            {nombre:"Rosa Luxemburgo", telefono: "0987654321", curso:"Primero ESO"},
-            {nombre:"Alberto Herrera", telefono: "1122334455", curso:"Segundo ESO"},
-            {nombre:"Ana Mariño", telefono: "6677889900", curso:"Tercero ESO"}
-            ];
+        $scope.alumnos=[
+                {nombre:"Juan Blanco", telefono: "1234567890", curso:"Segundo ESO"},
+                {nombre:"Rosa Luxemburgo", telefono: "0987654321", curso:"Primero ESO"},
+                {nombre:"Alberto Herrera", telefono: "1122334455", curso:"Segundo ESO"},
+                {nombre:"Ana Mariño", telefono: "6677889900", curso:"Tercero ESO"}
+                ];
 
-$scope.Save = function(){
-        $scope.alumnos.push({nombre: $scope.nuevoAlumno.nombre, telefono: $scope.nuevoAlumno.telefono, curso: $scope.nuevoAlumno.curso})
-}
-            
+        $scope.Save = function(){
+                $scope.alumnos.push({nombre: $scope.nuevoAlumno.nombre, telefono: $scope.nuevoAlumno.telefono, curso: $scope.nuevoAlumno.curso});
+                $scope.formVisibility=false;
+                console.log($scope.formVisibility)
+        }
+
+        $scope.formVisibility=false;
+
+        $scope.ShowForm=function(){
+
+	$scope.formVisibility=true;
+	console.log($scope.formVisibility)
+	
+        }
 }
